b8d0d90afe35ef787ec53dea2980605ccc4a226d
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-19 22:01:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+if array.size == 1
+  array.join
+elsif array.size == 2
+  array.join(" and ")
+elsif array.size >= 3
+  last = array.pop
+  array.join(", ") << ", and #{last}"
+  end
 end
