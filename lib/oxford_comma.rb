25e20636a569fc1562de667a8d3da09820fcd7e8
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-19 21:30:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
 if array.size == 1
   array.join
-end
-if array.size == 2
+elsif array.size == 2
   array.join(" and ")
-end
+elsif array.size >= 3
+  last = array.pop
+  array.join(", ") << "and #{last}"
+  end
 end
